43977ec85034d9caad597e82316e574a56eef2e7
ethanyadabyte <ethan.vdm@deepcode.xyz>
2021-02-21 11:02:00 -0800

    ScrollBar added to itempage

--- src/Components/CastList.tsx
+++ src/Components/CastList.tsx
@@ -12,22 +12,24 @@ interface CastProps {
 
 export const CastList = (props: CastProps) => {
   return (
-    <ul className="flex flex-linare ml-24  ">
-      {props.results.map((cast) => (
-        <li className="my-4 px-10 text-gray-200 rounded-lg ml-2 ">
-          <img
-            className="h- rounded-2xl "
-            src={"https://image.tmdb.org/t/p/w154" + cast.profile_path}
-            alt=""
-          />
-          <div className="ml-2">
-            <p className="text-base mt-2 w-32 p-0 font-medium ">
-              {cast.character}
-            </p>
-            <p className="text-base w-32 text-gray-400 ">{cast.name}</p>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <div className="  mt-4 overflow-x-auto scrollbar-w-4 scrollbar-track-gray-lighter scrollbar-thumb-rounded-2xl scrollbar-thumb-black scrolling-touch w-auto mx-36  rounded-2xl">
+      <ul className="flex flex-linare   ">
+        {props.results.map((cast) => (
+          <li className="my-4 px-8 text-gray-200 rounded-lg  ml-1">
+            <img
+              className=" rounded-2xl "
+              src={"https://image.tmdb.org/t/p/w154" + cast.profile_path}
+              alt=""
+            />
+            <div className="ml-2">
+              <p className="text-base mt-2 w-32 p-0 font-medium ">
+                {cast.character}
+              </p>
+              <p className="text-base w-32 text-gray-400 ">{cast.name}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
