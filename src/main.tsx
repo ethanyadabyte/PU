cc0031af2fddacc656828afe4be0d8ef0410e521
ethanyadabyte <ethan.vdm@deepcode.xyz>
2021-02-23 13:07:33 -0800

    Inti V1.5

--- src/main.tsx
+++ src/main.tsx
@@ -10,30 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-/*import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./App";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-*/
-/*
-First Build
-
-import React from "react";
-import ReactDOM from "react-dom";
-import "tailwindcss/dist/tailwind.min.css";
-import App from "./App";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-*/
